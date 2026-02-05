4b6c004708e42139b4bb8fea6fed5af5083c76f6
Vedashree MJ <vedashreemj61@gmail.com>
2026-02-06 00:53:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const text = "Software Engineering Fresher";
+const text = "DevOps & Software Engineer";
 let i = 0;
 
 function typeEffect() {
   if (i < text.length) {
     document.getElementById("typing").innerHTML += text.charAt(i);
     i++;
-    setTimeout(typeEffect, 100);
+    setTimeout(typeEffect, 90);
   }
 }
+
 typeEffect();
